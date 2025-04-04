64fe658cd7474560ece54167fe25bcfbbf3a74d9
Cerzo-Mark <mvcerzo@paterostechnologicalcollege.edu.ph>
2025-04-04 13:48:48 +0800

    Cerzo.java

---
+++ Cerzo.java
@@ -0,0 +1,8 @@
+public class Cerzo {
+  public static display() {
+    int age = 21;
+    System.out.println("Hello! This is Cerzo contributing to the project");
+    System.out.println("My current age is: " + age);
+    System.out.println("Next year, I will be: " + (age = 1));
+  }
+}
